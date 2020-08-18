0c9caefcf3360921bc6eaa40dc5bdc0f64141b5d
kodlon <45404949+kodlon@users.noreply.github.com>
2020-08-18 11:44:47 +0300

    Динамічний масив

---
+++ all_files/pidkotovka23_Dynamic_array/pidkotovka23.c
@@ -0,0 +1,67 @@
+/*Лінійний пошук, бульбашка. Але з динамічним масивом*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void bubble_sort(double *array, int n);
+double linear_search(double *array, int n);
+void print_array(double *array, int n);
+void write_array(double *array, int n);
+
+int main(){
+  int n;
+  double *array;
+
+  scanf("%d", &n);
+
+  for (int i = 0; i < n; i++) {
+    array = (double*) malloc(n * sizeof(double));
+  }
+
+  write_array(array, n);
+  bubble_sort(array, n);
+  printf("Sorted:\n");
+  print_array(array, n);
+  linear_search(array, n);
+  free(array);
+  
+  return 0;
+}
+
+void print_array(double *array, int n){
+  for (int i = 0; i < n; i++) {
+    printf("%lf\n", array[i]);
+  }
+}
+
+void write_array(double *array, int n){
+  for (int i = 0; i < n; i++) {
+    scanf("%lf", &array[i]);
+  }
+}
+
+void bubble_sort(double *array, int n){
+  int i = 0, tmp;
+  while (i < n - 1) {
+    if (array[i] >= array[i + 1]) {
+      tmp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = tmp;
+      i = 0;
+    }
+    else
+      ++i;
+  }
+}
+
+double linear_search(double *array, int n){
+  printf("Key?\n");
+  double x;
+  scanf("%lf", &x);
+  for (int i = 0; i < n; i++) {
+    if(array[i] == x){
+      printf("key is: %g; index in array: %d;", x, i);
+      break;
+    }
+  }
+}
